b44db120db88108c19c29ab4049a6ba4cb33ffa3
Ravindernath <83633494+Ravindernath2960@users.noreply.github.com>
2022-07-23 17:32:34 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+import spacy 
+def get_score(text1,text2):
+    nlp = spacy.load('en_core_web_lg')
+    doc1 = nlp(text1)
+    doc2 = nlp(text2)
+    print(doc1.similarity(doc2))
+    
+i = 1
+while i != 0:
+    text1 = input('please enter the text 1')
+    text2 = input('please enter the text 2')
+    get_score(text1,text2)
+    i = int(input('enter 1 to check for a new sentence. to exit enter 0'))
